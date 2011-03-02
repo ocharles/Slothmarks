934ae0101f7fecded2c683af1e571bb0dafeef37
Oliver Charles <oliver.g.charles@googlemail.com>
2011-03-02 18:23:58 +0000

    Add a context to our application.
    Our methods are going to touch the DB, so we need a convenient way to pass a
    database handle around. As our schema is pretty straightforward, I'm going to
    use DBIx::Class to abstract the database out.
    
    I packaged this context up in the Slothmarks module, and instantiate it in our
    REST server. This allows me to access the Slothmarks context outside the REST
    server, for example I may need it later in cron jobs, or scripts.

--- lib/Slothmarks.pm
+++ lib/Slothmarks.pm
@@ -1,2 +1,20 @@
 package Slothmarks;
+use Moose;
+use namespace::autoclean;
+
+use Slothmarks::Schema;
+
+has schema => (
+    is => 'ro',
+    default => sub {
+        Slothmarks::Schema->connect(
+            'dbi:Pg:dbname=slothmarks',
+            'slothmarks',
+            '',
+            { RaiseError => 1 }
+        );
+    }
+);
+
+__PACKAGE__->meta->make_immutable;
 1;
--- lib/Slothmarks/REST.pm
+++ lib/Slothmarks/REST.pm
@@ -2,7 +2,16 @@ package Slothmarks::REST;
 use Moose;
 use namespace::autoclean;
 
+use Slothmarks;
+
 extends 'Sloth';
+
+has '+c' => (
+    is => 'ro',
+    default => sub {
+        Slothmarks->new;
+    }
+);
 
 __PACKAGE__->meta->make_immutable;
 1;
