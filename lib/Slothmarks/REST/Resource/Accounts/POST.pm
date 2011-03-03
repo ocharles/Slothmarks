a0dfb62b47feb506cafc168366183e84a7fb3c44
Oliver Charles <oliver.g.charles@googlemail.com>
2011-03-03 00:49:16 +0000

    Implement account creation.
    As we mentioned before, a POST to /accounts should 'append' a new account to the
    account list, which means registering a new account. We first check for an
    already existing account with a given username -- if that's taken we return a
    409 conflict error.
    
    Otherwise, we insert the row, and then 'throw' the created message. This skips
    any further serialization, and adds the Location header. We use
    $request->uri_for to generate a URI based on various path components.

--- lib/Slothmarks/REST/Resource/Accounts/POST.pm
+++ lib/Slothmarks/REST/Resource/Accounts/POST.pm
@@ -4,8 +4,59 @@ use namespace::autoclean;
 
 with 'Sloth::Method';
 
+use Data::TreeValidator::Constraints qw( required );
+use Data::TreeValidator::Sugar qw( leaf branch );
+use HTTP::Status qw( HTTP_CREATED );
+use HTTP::Throwable::Factory 'http_throw';
+use Sloth::RequestParser::Form;
+use Try::Tiny;
+
+use aliased 'Sloth::Response';
+
+has '+request_data_validator'  => (
+    default => sub {
+        branch {
+            user_id   => leaf(constraints => [ required ]),
+            password  => leaf(constraints => [ required ]),
+            full_name => leaf(),
+        }
+    }
+);
+
+has '+request_parsers' => (
+    default => sub {
+        return {
+            'application/x-www-form-urlencoded' => Sloth::RequestParser::Form->new
+        }
+    }
+);
+
 sub execute {
-    die 'Not yet implemented';
+    my ($self, $input, $request) = @_;
+
+    if ($self->c->schema->resultset('User')->find($input->{user_id})) {
+        http_throw('Conflict' => {
+            message => 'An account with this name already exists'
+        });
+    }
+    else {
+        my $user = try {
+            $self->c->schema->resultset('User')->create($input);
+        }
+        catch {
+            http_throw('BadRequest' => { message => $_ });
+        };
+
+        return Response->new(
+            HTTP_CREATED,
+            [
+                Location => $request->uri_for(
+                    resource => 'user',
+                    name => $user->user_id
+                )
+            ]
+        );
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
